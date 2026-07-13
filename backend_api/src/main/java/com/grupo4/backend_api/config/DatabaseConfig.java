9850503837915e7c67a7541d3482721ad168e8d1
Jota2105 <150400932+Jota2105@users.noreply.github.com>
2026-07-12 22:55:19 -0500

    fix(backend): alinear datasource con cluster PostgreSQL

--- backend_api/src/main/java/com/grupo4/backend_api/config/DatabaseConfig.java
+++ backend_api/src/main/java/com/grupo4/backend_api/config/DatabaseConfig.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.grupo4.backend_api.config;
 
-/**
- *
- * @author dcobe
- */
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @DataSourceDefinition(
-    name = "java:global/jdbc/NexoDS",          // Nombre JNDI estandarizado globalmente
+    name = "java:global/jdbc/NexoDS",
     className = "org.postgresql.ds.PGSimpleDataSource",
-    user = "usuario_nexo",                         // Tu usuario
-    password = "admin123",                     // Tu contraseña
-    databaseName = "nexo_db",                  // Tu BD
+    user = "postgres",
+    password = "postgres",
+    databaseName = "nexo_db",
     serverName = "haproxy",
     portNumber = 5432,
     minPoolSize = 2,
@@ -24,6 +16,4 @@ import jakarta.enterprise.context.ApplicationScoped;
 )
 @ApplicationScoped
 public class DatabaseConfig {
-    // Esta clase puede estar vacía. 
-    // Su único propósito es alojar la anotación para que el servidor la lea al arrancar.
 }
